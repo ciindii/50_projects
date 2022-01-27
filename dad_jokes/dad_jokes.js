50c33ff163204d57747cf864f1bd8ca3ef1e9e9c
Cindy Truong <cindythitruong@gmail.com>
2022-01-27 06:07:27 -0800

    updated .then() to async/await

--- dad_jokes/dad_jokes.js
+++ dad_jokes/dad_jokes.js
@@ -1,19 +1,38 @@
-const jokeEL = document.getElementById('joke')
+
+   
+const jokeEl = document.getElementById('joke')
 const jokeBtn = document.getElementById('jokeBtn')
 
 jokeBtn.addEventListener('click', generateJoke)
 
 generateJoke()
 
-function generateJoke() {
+// USING ASYNC/AWAIT
+async function generateJoke() {
   const config = {
     headers: {
       Accept: 'application/json',
     },
   }
-  fetch('https://icanhazdadjoke.com', config)
-    .then((res) => res.json())
-    .then((data) => {
-      jokeEL.innerHTML = data.joke
-    })
+
+  const res = await fetch('https://icanhazdadjoke.com', config)
+
+  const data = await res.json()
+
+  jokeEl.innerHTML = data.joke
 }
+
+// USING .then()
+// function generateJoke() {
+//   const config = {
+//     headers: {
+//       Accept: 'application/json',
+//     },
+//   }
+
+//   fetch('https://icanhazdadjoke.com', config)
+//     .then((res) => res.json())
+//     .then((data) => {
+//       jokeEl.innerHTML = data.joke
+//     })
+// }
